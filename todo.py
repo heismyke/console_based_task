16d3272aee14802d713264a745459a24621ef853
myke <81974207+heismyke@users.noreply.github.com>
2023-10-01 14:07:50 +0100

    todopy

---
+++ todo.py
@@ -0,0 +1,38 @@
+import uuid
+import json
+
+# Load existing tasks or initialize an empty list
+filename = "./tasks.json"
+try:
+    with open(filename, "r") as json_file:
+        tasks = json.load(json_file)
+except FileNotFoundError:
+    tasks = []
+
+while True:
+    input_value = input("Enter a task (or 'quit' to exit): ")
+
+    if input_value.strip() == "quit":
+        input_yes = input("Are you sure you want to quit? (yes/no): ")
+        if input_yes.lower() == "yes":
+            break
+    elif input_value:
+        task = {
+            "id": str(uuid.uuid4()),
+            "title": input_value
+        }
+        tasks.append(task)
+
+# Save the updated tasks to the JSON file
+with open(filename, "w") as json_file:
+    json.dump(tasks, json_file)
+
+# Print the tasks
+def load_tasks(filename):
+    with open(filename, "r") as json_file:
+        data = json.load(json_file)
+        for task in data:
+            print(f'The id is {task["id"]} and task is {task["title"]}')
+        return data
+
+loaded_tasks = load_tasks(filename)
